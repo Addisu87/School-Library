d4cd51c430ca6a099c87c5d6a38d2ee72f120209
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-08-23 00:41:03 +0300

    Create class files

---
+++ person.rb
@@ -0,0 +1,23 @@
+class Person 
+  def initialize(id, age, name = 'Unknown', parent_permission: true)
+    @id = id
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name, :age
+
+  # public method
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  # private method
+  private
+
+  def is_of_age?
+    @age >= 18
+  end
+end
